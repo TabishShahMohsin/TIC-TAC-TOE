09f14aa29b13a66b8ffbb620b4306f02cb0c84f0
TabishShahMohsin <tabishshah0786@gmail.com>
2024-12-30 13:31:36 +0530

    Done almost

--- experiment.py
+++ experiment.py
@@ -1,28 +1,3 @@
-class something:
-    def __init__(self):
-         self.state=['x', 'x', 'o', 'o', 'x', 'x', 'x', 'o', 'o']
-         self.status=self.check_who_won()
-    def __str__(self):
-        return self.check_who_won()
-    def check_who_won(self):
-            
-                # Checking 3 consecutive horizontally, vertically and diagonally respectively
-            for i in range(3):
-                if self.state[0+3*i]==self.state[1+3*i]==self.state[2+3*i]:
-                    print("abcd")
-                    return 1  if self.state[0+3*i]=="x" else -1
-                elif self.state[0+i]==self.state[3+i]==self.state[6+i]:
-                    print("efg")
-
-                    return 1  if self.state[0+i]=="x" else -1
-            for i in range(2):
-                if self.state[6*i]==self.state[4]==self.state[-6*i+8]:
-                    print("hij")
-                    return 1  if self.state[8*i]=="x" else -1
-            # Checking if all chances are exhausted: 0 corresponds to draw
-            if self.state.count("x")+self.state.count("o")==9:
-                return 0
-                
-            # If more chances can be played: 2
-            return 2
-print(something().status)
+from projectpp import Game
+tree=Game([" ", " ", " ", " ", " ", " ", " ", " ", " "],"x")
+print(tree.status)
--- projectpp.py
+++ projectpp.py
@@ -12,15 +12,9 @@ class Game:
         self.value=self.get_minimax()
         self.min=-1
         self.max=1
+        self.children=self.sort()
         
-        
-    def Print(self):
-        for i in range(3):
-            for j in range(3):
-                print(f"| {self.state[j+i*3]} ", end="")
-            print("|")
-            if i!=2:   
-                print(" ___"*3+"\n") 
+     
                 
                 
     def check_who_won(self):
@@ -28,12 +22,22 @@ class Game:
                 # Checking 3 consecutive horizontally, vertically and diagonally respectively
             for i in range(3):
                 if self.state[0+3*i]==self.state[1+3*i]==self.state[2+3*i]:
-                    return 1  if self.state[0+3*i]=="x" else -1
+                    if self.state[0+3*i]=="x":
+                        return 1
+                    elif self.state[0+3*i]=="o":
+                        return -1
                 elif self.state[0+i]==self.state[3+i]==self.state[6+i]:
-                    return 1  if self.state[0+i]=="x" else -1
+                    if self.state[0+i]=="x":
+                        return 1
+                    elif self.state[0+i]=="o":
+                        return -1
+
             for i in range(2):
                 if self.state[6*i]==self.state[4]==self.state[-6*i+8]:
-                    return 1  if self.state[8*i]=="x" else -1
+                    if self.state[6*i]=="x":
+                        return 1
+                    elif self.state[6*i]=="o":
+                        return -1
             # Checking if all chances are exhausted: 0 corresponds to draw
             if self.state.count("x")+self.state.count("o")==9:
                 return 0
@@ -59,11 +63,33 @@ class Game:
             return self.status
         for i in self.children:
             values.append(i.value)
+        self.min=min(values)
+        self.max=max(values)
         if self.chance=="x":
-            return max(values)
+            return self.max
         else:
-            return min(values)
-                
+            return self.min
+        
+        
+    def sort(self):
+        sorted_children=[[],[],[]]
+        for i in self.children:
+            sorted_children[i.value+1].append(i)
+        return sorted_children        
+    
+    
+    def move(self):
+        for i in range(3):
+            try:
+                if self.chance=="o":
+                    recommended_move=random.choice(self.children[i])
+                else:
+                    recommended_move=random.choice(self.children[2-i])
+            except IndexError:
+                pass
+            else:
+                break
+        return recommended_move
             
         
         
@@ -74,78 +100,92 @@ class Game:
 
 def main():
     
+    # Clearing the terminal before start
     os.system("cls" if os.name == "nt" else "clear")
+    
     
     # Just to show the user the grid marking
     printing_grid=[]
     for i in range(3):
         for j in range(3):
-            printing_grid.append(i*3+j+1)
-
-    Game(printing_grid).Print
+             printing_grid.append(i*3+j+1)
+    Print(printing_grid)
     
     print("\nWelcome to TicTacToe!")
     
+    tree=Game([" ", " ", " ", " ", " ", " ", " ", " ", " "],"x")
     
-    # Asking the user to choose between x and o
+    
+    # Asking the user to choose for x or o
     while True:
         choice=input("Would you like to play enter x or o: ")
         if choice in ["x", "o"]:
             break
     
     
+    next_move=tree
+    
     
     # Performing the actual grid loop till the game ends, (game-loop)
     while True:
+        # CLearing the window before every play
+        
+        
         if choice=="o":
             # Getting the best move for x
-            
-            grid=max_ll(minimax(grid, grid_width, winning_length))
+            next_move=next_move.move()
             #Printing the grid to the user again and again
-            print_grid(grid)
-            
-            state=check_who_won(grid, grid_width, winning_length)
-            if state!=2:
+            # next_move.Print()
+            Print(next_move.state)
+    
+            if next_move.status!=2:
                 break
         
-        
-        if player_move(choice,grid, grid_width)=="undo":
-            grid=prev_grid
-        else:
-            prev_grid=list(grid)
+        # Player move:
+        player=player_move(list(next_move.state),choice)
+        for i in next_move.children:
+            for j in i:
+                if player==j.state:
+                    next_move=j
+          
         os.system("cls" if os.name == "nt" else "clear")
-        
-          
-          
+
         # Ending the game if win/lose/draw
-        state=check_who_won(grid, grid_width, winning_length)
-        if state!=2:
+        if next_move.status!=2:
             break
           
           
             
         if choice=="x":
         # Storing the optimal move for o in l
-            grid=min_ll(minimax(grid, grid_width, winning_length))
+            next_move=next_move.move()
         
         #Printing the grid to the user again and again
-            print_grid(grid)
+            # next_move.Print()
+            Print(next_move.state)
             # Ending the game if win/lose/draw
-            state=check_who_won(grid, grid_width, winning_length)
-            if state!=2:
+            if next_move.status!=2:
                 break 
         
     # The player will never win, In Sha ALLAH 
-    if state!=0:
+    # next_move.Print()
+    if next_move.status!=0:
         print("Computer Wins!!")
     else:
         print("It's Draw!, you'll never win against me!")
-    print(grid)
+    # next_move.Print()
+    Print(next_move.state)
 
 
         
-
-
+# Making a printing funciton:
+def Print(table):
+    for i in range(3):
+        for j in range(3):
+            print(f"| {table[j+i*3]} ", end="")
+        print("|")
+        if i!=2:   
+            print(" ___"*3+"\n")
 
 
 
@@ -160,7 +200,7 @@ def player_move(grid, chance):
         try:
             inp=int(input(f"Enter from 1 to 9: "))
             inp=inp-1
-        except:
+        except ValueError:
             continue
         
         if grid[inp]==" " and chance=="x":
@@ -169,9 +209,9 @@ def player_move(grid, chance):
         if grid[inp]==" " and chance=="o":
             grid[inp]="o"
             break
+    return grid
         
         
 
 if __name__=="__main__":
     main()
-    
